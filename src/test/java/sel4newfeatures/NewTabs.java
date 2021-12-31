591ddd211c377bc5826cc6a0a5599bc654d2e149
techproed <abayram.techproed@gmail.com>
2021-12-31 09:53:19 -0600

    windows update

--- src/test/java/sel4newfeatures/NewTabs.java
+++ src/test/java/sel4newfeatures/NewTabs.java
@@ -1,36 +1,51 @@
 package sel4newfeatures;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.io.File;
 import java.io.IOException;
 
 public class NewTabs {
 
     @Test
-    public void newTabsTest() throws IOException {
+    public void newTabsTest() throws IOException, InterruptedException {
         //creating selenium driver
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         //opening google on teh first tab
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
+        String googleHandle=driver.getWindowHandle();
 
         //NEW WITH SELENIUM 4- opening a new TAB and going to linkedin
         WebDriver tab2 = driver.switchTo().newWindow(WindowType.TAB);
         tab2.get("https://www.linkedin.com");
         System.out.println(tab2.getTitle());
+        String linkedinHandle=driver.getWindowHandle();
 
         //NEW WITH SELENIUM 4- opening a new TAB and going to amazon
         WebDriver tab3 = driver.switchTo().newWindow(WindowType.TAB);
         tab2.get("https://www.amazon.com");
         System.out.println(tab3.getTitle());
+        String amazonHandle=driver.getWindowHandle();
 
+        //switching between the tabs
+        Thread.sleep(5000);
+        driver.switchTo().window(linkedinHandle);
+        System.out.println("LINKEDIN : "+tab2.getCurrentUrl());
+        Assert.assertTrue(tab2.getCurrentUrl().contains("linkedin"));
 
+        Thread.sleep(5000);
+        driver.switchTo().window(googleHandle);
+        System.out.println("GOOGLE : "+driver.getCurrentUrl());
+        Assert.assertTrue(driver.getCurrentUrl().contains("google"));
+
+        Thread.sleep(5000);
+        driver.switchTo().window(amazonHandle);
+        System.out.println("AMAZON : "+tab3.getCurrentUrl());
+        Assert.assertTrue(tab3.getCurrentUrl().contains("amazon"));
 
     }
 }
--- src/test/java/sel4newfeatures/NewWindows.java
+++ src/test/java/sel4newfeatures/NewWindows.java
@@ -1,6 +1,7 @@
 package sel4newfeatures;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
@@ -18,15 +19,33 @@ public class NewWindows {
         //window 1 go to google
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
+        String googleHandle=driver.getWindowHandle();
 
         //window 2 go linkedin
         WebDriver window2 = driver.switchTo().newWindow(WindowType.WINDOW);
         window2.get("https://www.linkedin.com");
         System.out.println(window2.getTitle());
+        String linkedinHandle=driver.getWindowHandle();
 
         //window 3 go to amazon
         WebDriver window3 = driver.switchTo().newWindow(WindowType.WINDOW);
         window3.get("https://www.amazon.com");
         System.out.println(window3.getTitle());
+        String amazonHandle=driver.getWindowHandle();
+        System.out.println(driver.getCurrentUrl());
+
+        //switching between the windows
+        driver.switchTo().window(linkedinHandle);
+        System.out.println("LINKEDIN : "+window2.getCurrentUrl());
+        Assert.assertTrue(window2.getCurrentUrl().contains("linkedin"));
+
+        driver.switchTo().window(googleHandle);
+        System.out.println("GOOGLE : "+driver.getCurrentUrl());
+        Assert.assertTrue(driver.getCurrentUrl().contains("google"));
+
+        driver.switchTo().window(amazonHandle);
+        System.out.println("AMAZON : "+window3.getCurrentUrl());
+        Assert.assertTrue(window3.getCurrentUrl().contains("amazon"));
+
     }
 }
